c9ab82175a078b8928ecd24f7b4d871439d5bd75
sebcamp11 <105763585+gxboy11@users.noreply.github.com>
2023-11-22 02:13:00 -0600

    Carritos Vista

---
+++ Proyecto.Web/Controllers/CarritosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto.Web.Controllers
+{
+
+    public class CarritoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+
+}
